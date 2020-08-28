c7b24b854c17cdc6bf595a9ddbcc039a99947192
EricBenditt <erb5623@psu.edu>
2020-08-28 19:22:11 +0000

    Added the degree sign.

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 celsius = float(input ("Enter temperature in celsius: ")) 
 fahrenheit = (celsius * 1.8) + 32
-print('%.2f in Celsius is equivalent to %0.2f Fahrenheit.' %(celsius, fahrenheit))  
+print('%.2f\N{degree sign} in Celsius is equivalent to %0.2f\N{degree sign} Fahrenheit.' %(celsius, fahrenheit))  
