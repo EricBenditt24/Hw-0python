bd5eaa3a5964f2bdb82117b5989142c1a8ef37ac
EricBenditt <erb5623@psu.edu>
2020-08-28 19:59:32 +0000

    Edited the coding of the decimal

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 celsius = float(input ("Enter temperature in celsius: ")) 
 fahrenheit = (celsius * 1.8) + 32
-print('%.1f\N{degree sign} in Celsius is equivalent to %0.1f\N{degree sign} Fahrenheit.' %(celsius, fahrenheit))   
+print('%s\N{degree sign} in Celsius is equivalent to %s\N{degree sign} Fahrenheit.' %(celsius, fahrenheit))   
